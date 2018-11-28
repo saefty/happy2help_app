f3330bb5d99dc4e75ab92ac0094f724a0998a6ea
Saef Taher <tahersaef@gmail.com>
2018-11-28 23:14:37 +0100

    Add new event screen

---
+++ src/components/event/edit.event.style.js
@@ -0,0 +1,22 @@
+// @flow
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        marginTop: 15,
+        padding: 20,
+        width: '100%',
+    },
+    bannerWrapper: {
+        alignItems: 'center',
+    },
+    banner: {
+        width: '70%',
+        marginBottom: 15,
+    },
+    button: {
+        zIndex: -1,
+        backgroundColor: '#ffffff',
+    },
+});
---
+++ src/components/location/GooglePlacesGeolocation.js
@@ -0,0 +1,64 @@
+// @flow
+import React, { Component } from 'react';
+import {
+  View,
+} from 'react-native';
+import Qs from 'qs';
+import Bottleneck from "bottleneck";
+
+export class GooglePlacesAutocomplete extends Component<any, any> {
+  _isMounted = true;
+  _results = [];
+  _requests = [];
+  limiter = new Bottleneck({
+    maxConcurrent: 1,
+    minTime: 333
+  });
+
+  constructor (props: any) {
+    super(props);
+  }
+
+  async componentDidMount() {
+    await this.limiter.ready()
+  }
+
+  async componentWillUnmount() {
+    await this.limiter.stop()
+  }
+
+  search = async (text: string) => {
+    return this.limiter.schedule(
+        async () => {
+            return fetch('https://maps.googleapis.com/maps/api/place/autocomplete/json?&input=' + encodeURIComponent(text) + '&' + Qs.stringify(this.props.query))
+            .then((response) => {
+              return response.json()
+            })
+        }
+    );
+  }
+
+  fetchCoordinates = async (place_id: string) => {
+    return this.limiter.schedule(
+      async () => {
+        return fetch('https://maps.googleapis.com/maps/api/place/details/json?' + Qs.stringify({
+          key: this.props.query.key,
+          placeid: place_id,
+          language: this.props.query.language,
+        })).then((response) => response.json()) 
+      }
+    );
+  }
+
+
+  render() {
+    return (
+      <View>
+        {this.props.children({
+            search: this.search,
+            fetchCoordinates: this.fetchCoordinates
+        })}
+      </View>
+    );
+  }
+}
