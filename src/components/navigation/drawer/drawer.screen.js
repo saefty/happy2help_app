c9a031dcf5084e6c841483469a33b35c918656e1
Saef Taher <tahersaef@gmail.com>
2018-12-29 15:03:16 +0100

    Orga reload fix

--- src/components/navigation/drawer/drawer.screen.js
+++ src/components/navigation/drawer/drawer.screen.js
@@ -87,6 +87,7 @@ class _DrawerScreen extends React.Component<any, any> {
                             `}
                         >
                             {({ error, loading, data, refetch }) => {
+                                if (this.props.navigation.state.isDrawerOpen) refetch();
                                 if (error || loading) return <View />;
                                 return (
                                     <List.Accordion
@@ -108,7 +109,6 @@ class _DrawerScreen extends React.Component<any, any> {
                                         }
                                         left={props => <List.Icon {...props} color="#000" icon="group" />}
                                     >
-                                        <NavigationEvents onWillFocus={refetch} />
                                         <List.Item
                                             onPress={() => this.props.navigation.navigate('EditOrganisation')}
                                             left={props => <List.Icon {...props} icon="group-add" />}
