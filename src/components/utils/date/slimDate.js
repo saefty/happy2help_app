fec70cd2c85b45cd629177ee3f5fda1a97e91362
David Schach <schachdavid@web.de>
2019-01-03 13:08:14 +0100

    added moment

--- src/components/utils/date/slimDate.js
+++ src/components/utils/date/slimDate.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { styles } from './slimDate.style';
+import moment from 'moment';
 
 type Props = {
     t: i18n.t,
@@ -15,39 +16,15 @@ export class SlimDate extends Component<Props> {
         super(props);
     }
 
-    getMonthString = (monthIndex: number) => {
-        switch (monthIndex+1) {
-            case 1:
-                return 'JAN';
-            case 2:
-                return 'FEB';
-            case 3:
-                return 'MÄR';
-            case 4:
-                return 'APR';
-            case 5:
-                return 'MAI';
-            case 6:
-                return 'JUN';
-            case 7:
-                return 'JUL';
-            case 8:
-                return 'AUG';
-            case 9:
-                return 'SEP';
-            case 10:
-                return 'OKT';
-            case 11:
-                return 'NOV';
-            case 12:
-                return 'DEZ';
-        }
-    };
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.day}> {this.props.date.getDate()} </Text>
-                <Text style={{ color: this.props.accentColor !== undefined ? this.props.accentColor : 'red' }}> {this.getMonthString(this.props.date.getMonth())} </Text>
+                <Text style={{ color: this.props.accentColor !== undefined ? this.props.accentColor : 'red' }}>
+                    {moment(this.props.date)
+                        .format('MMM')
+                        .toUpperCase()}
+                </Text>
             </View>
         );
     }
