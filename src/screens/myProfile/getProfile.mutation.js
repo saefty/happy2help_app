f623f29fcb6e2b3f4aba26f42fb9cd9692ed5f00
Saef Taher <tahersaef@gmail.com>
2019-01-30 11:55:59 +0100

    Hot fix :)

--- src/screens/myProfile/getProfile.mutation.js
+++ src/screens/myProfile/getProfile.mutation.js
@@ -2,10 +2,10 @@ import gql from 'graphql-tag';
 import { BASE_USER } from '../../fragments';
 
 export const GET_PROFILE = gql`
-    {
+    query {
         user {
-           ...BASE_USER
+            ...BASE_USER
         }
-        ${BASE_USER}
     }
+    ${BASE_USER}
 `;
